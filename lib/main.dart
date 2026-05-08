45d6d9bae271746be3ec99421fbf95e93a15f15b
abdulsattarkhokhar444 <abdulsattarkhokhar444@gmail.com>
2026-05-08 16:17:38 +0000

    fix: enable firebase with google-services.json

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ import 'dart:ui';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
- // await Firebase.initializeApp(
-  //  options: DefaultFirebaseOptions.currentPlatform,
- // );
+  await Firebase.initializeApp(
+    //options: DefaultFirebaseOptions.currentPlatform,
+  );
   runApp(const HisaabProApp());
 }
 
