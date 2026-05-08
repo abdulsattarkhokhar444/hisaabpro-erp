425a1b9d35be5b6547e7fe2d6fb59fef679c4b0f
abdulsattarkhokhar444 <abdulsattarkhokhar444@gmail.com>
2026-05-08 15:12:13 +0000

    test without firebase

--- lib/main.dart
+++ lib/main.dart
@@ -3,10 +3,13 @@ import 'package:firebase_core/firebase_core.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:google_fonts/google_fonts.dart';
 import 'dart:ui';
+import 'firebase_options.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+ // await Firebase.initializeApp(
+  //  options: DefaultFirebaseOptions.currentPlatform,
+ // );
   runApp(const HisaabProApp());
 }
 
